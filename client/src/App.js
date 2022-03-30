886b6dbc6a54a7b97e97953a617772bb20953d6a
hajame <harri.mehtala@helsinki.fi>
2022-03-30 15:01:41 +0300

    client-server communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [greeting, setGreeting] = useState("not connected");
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/hello").then((response) => {
+      setGreeting(response.data.content);
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Räkkeri</p>
+        <p>{greeting}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- server/src/main/java/rakkeri/rakkeri_server/RakkeriServerApplication.java
+++ server/src/main/java/rakkeri/rakkeri_server/RakkeriServerApplication.java
@@ -2,12 +2,25 @@ package rakkeri.rakkeri_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RakkeriServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RakkeriServerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/hello").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
