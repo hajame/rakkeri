bd852ee68966f443b07cda5d807be495d1d0f141
hajame <harri.mehtala@helsinki.fi>
2022-04-07 10:24:29 +0300

    hooks for fields

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,12 +1,33 @@
 import { useState, useEffect } from "react";
 
 export const SignUpForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="App">
       <form className="App-header">
-        <input type="text"></input>
-        <input type="password"></input>
-        <button>Sign Up</button>
+        <div>Create a new account</div>
+        <p></p>
+        <div>
+          username{" "}
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password{" "}
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   );
