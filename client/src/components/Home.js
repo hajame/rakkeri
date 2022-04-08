ff75e01a64912467fc78ffe9939f8965d8581a7a
hajame <harri.mehtala@helsinki.fi>
2022-04-08 11:28:29 +0300

    user is saved to localStorage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import userService from "../services/users";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
@@ -16,16 +16,90 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 export const Home = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const userJSON = window.localStorage.getItem("rakkeriAppUser");
+    if (userJSON) {
+      const user = JSON.parse(userJSON);
+      setUser(user);
+      userService.setToken(user.token);
+    }
+  }, []);
+
+  const loginForm = () => (
+    <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+      <Typography component="h1" variant="h5">
+        Sign in to Räkkeri
+      </Typography>
+
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="username"
+        value={username}
+        label="Username"
+        name="username"
+        autoComplete="username"
+        autoFocus
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        value={password}
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Sign in
+      </Button>
+      <Grid container>
+        <Grid item>
+          <Link href="/signup" variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Grid>
+        {/* Todo: */}
+        {/* <Grid item xs>
+          <Link href="" variant="body2">
+            Forgot password?
+          </Link>
+        </Grid> */}
+      </Grid>
+    </Box>
+  );
+
+  const projectView = () => (
+    <Box component="form" noValidate sx={{ mt: 1 }}>
+      <Typography component="h1" variant="h5">
+        Welcome {user.username}
+      </Typography>
+
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        New project
+      </Button>
+    </Box>
+  );
 
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const response = await userService.login({ username, password });
+      const user = await userService.login({ username, password });
+      setUser(user);
+      userService.setToken(user.token);
+      window.localStorage.setItem("rakkeriAppUser", JSON.stringify(user));
+      setUsername("");
+      setPassword("");
     } catch (e) {
       console.error("Error when logging in", e);
     }
-    setUsername("");
-    setPassword("");
   };
 
   return (
@@ -40,57 +114,8 @@ export const Home = () => {
           alignItems: "center",
         }}
       >
-        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
-          <Typography component="h1" variant="h5">
-            Sign in to Räkkeri
-          </Typography>
-
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            value={username}
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-            onChange={({ target }) => setUsername(target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            value={password}
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign in
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-            {/* Todo: */}
-            {/* <Grid item xs>
-              <Link href="" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid> */}
-          </Grid>
-        </Box>
+        {user === null && loginForm()}
+        {user !== null && projectView()}
       </Box>
     </Container>
     // </ThemeProvider>
--- client/src/services/users.js
+++ client/src/services/users.js
@@ -2,6 +2,8 @@ import axios from "axios";
 const usersUrl = `${process.env.REACT_APP_BACKEND_URL}/api/users`;
 const loginUrl = `${process.env.REACT_APP_BACKEND_URL}/api/login`;
 
+let token = null;
+
 const create = async (credentials) => {
   const response = await axios.post(usersUrl, credentials);
   return response;
@@ -9,7 +11,11 @@ const create = async (credentials) => {
 
 const login = async (credentials) => {
   const response = await axios.post(loginUrl, credentials);
-  return response;
+  return response.data;
 };
 
-export default { create, login };
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
+export default { create, login, setToken };
