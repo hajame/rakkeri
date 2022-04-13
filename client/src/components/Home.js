076a3dfd497e6cac7ec8a6d606213f6200884606
hajame <harri.mehtala@helsinki.fi>
2022-04-13 14:04:45 +0300

    extract project list view

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,20 +3,10 @@ import userService from "../services/users";
 import projectService from "../services/projects";
 
 import LoginForm from "./LoginForm";
+import Projects from "./Projects";
 
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
@@ -43,38 +33,6 @@ export const Home = () => {
     updateState();
   }, []);
 
-  const projectView = () => (
-    <Box sx={{ mt: 1 }}>
-      <Typography component="h1" variant="h5">
-        Select a project
-      </Typography>
-      <List>
-        {projects.map((p) => (
-          <ListItem disablePadding key={p.id}>
-            <ListItemButton href={"projects/" + p.id}>{p.name}</ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Button
-        onClick={createProject}
-        fullWidth
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-      >
-        New project
-      </Button>
-    </Box>
-  );
-
-  const createProject = async () => {
-    const name = prompt("Please name your project", "name");
-    const response = await projectService.create(name, user);
-    const newProjects = response.projects.sort((a, b) =>
-      a.name.localeCompare(b.name)
-    );
-    setProjects(newProjects);
-  };
-
   return (
     // <ThemeProvider theme={theme}>
     <Container component="main" maxWidth="xs">
@@ -90,7 +48,9 @@ export const Home = () => {
         {user === null && (
           <LoginForm setUser={setUser} updateState={updateState} />
         )}
-        {user !== null && projectView()}
+        {user !== null && (
+          <Projects user={user} projects={projects} setProjects={setProjects} />
+        )}
       </Box>
     </Container>
     // </ThemeProvider>
---
+++ client/src/components/Projects.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import userService from "../services/users";
+import projectService from "../services/projects";
+
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import Button from "@mui/material/Button";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+const Projects = ({ user, projects, setProjects }) => {
+  const createProject = async () => {
+    const name = prompt("Please name your project", "name");
+    const response = await projectService.create(name, user);
+    const newProjects = response.projects.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setProjects(newProjects);
+  };
+
+  return (
+    <Box sx={{ mt: 1 }}>
+      <Typography component="h1" variant="h5">
+        Select a project
+      </Typography>
+      <List>
+        {projects.map((p) => (
+          <ListItem disablePadding key={p.id}>
+            <ListItemButton href={"projects/" + p.id}>{p.name}</ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Button
+        onClick={createProject}
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        New project
+      </Button>
+    </Box>
+  );
+};
+
+export default Projects;
