83c8eb02e5bee235fb5de329203572ba01b11f8c
hajame <harri.mehtala@helsinki.fi>
2022-05-19 10:11:58 +0300

    WIP reset email sending

--- server/src/main/java/rakkeri/rakkeri_server/service/EmailService.java
+++ server/src/main/java/rakkeri/rakkeri_server/service/EmailService.java
@@ -27,7 +27,6 @@ public class EmailService {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(host);
         mailSender.setPort(port);
-
         mailSender.setUsername(username);
         mailSender.setPassword(password);
 
